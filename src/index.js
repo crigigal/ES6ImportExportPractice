436e4a919aaa19769631f2d8127b9bc598d74342
crigigal <50228889+crigigal@users.noreply.github.com>
2021-07-10 21:00:20 +0100

    Exercise Updated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as operations from "./calculator";
 
 //Import the add, multiply, subtract and divide functions
 //from the calculator.js file.
@@ -7,10 +8,10 @@ import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <ul>
-    <li>{add(1, 2)}</li>
-    <li>{multiply(2, 3)}</li>
-    <li>{subtract(7, 2)}</li>
-    <li>{divide(5, 2)}</li>
+    <li>{operations.add(1, 2)}</li>
+    <li>{operations.multiply(2, 3)}</li>
+    <li>{operations.subtract(7, 2)}</li>
+    <li>{operations.divide(5, 2)}</li>
   </ul>,
   document.getElementById("root")
 );
